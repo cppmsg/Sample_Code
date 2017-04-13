89b3b797a3949ff20dbfc01184115cf616ddad0e
AlanU <blackcloak@gmail.com>
2017-04-13 10:03:38 -0500

    Update main.cpp
    Updated with more examples

--- unique_ptr/sample/main.cpp
+++ unique_ptr/sample/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
@@ -16,14 +17,64 @@ std::unique_ptr<char> modernFoo()
     return make_unique<char>('a');
 }
 
+ bool modernFooWithBool(std::shared_ptr<char> & myvar)
+{
+    try{
+         myvar = make_shared<char>('b');
+     }
+     catch( ...)
+     {
+         return false;
+     }
+     return true;
+}
+
+ bool modernUniqueFooWithBool(std::unique_ptr<char> & myvar)
+{
+    try{
+         myvar = make_unique<char>('c');
+     }
+     catch( ...)
+     {
+         return false;
+     }
+     return true;
+}
+
+ bool modernUniqueMultiFooWithBool(std::unique_ptr<char> & myvar,std::unique_ptr<char> & myvar2)
+{
+    try{
+         myvar = make_unique<char>('d');
+         myvar2 = make_unique<char>('e');
+     }
+     catch( ...)
+     {
+         return false;
+     }
+     return true;
+}
+
+
 int main(int argc, char *argv[])
 {
     char  * t = nullptr;
     foo(&t);
 
+    std::shared_ptr<char> sharemT ;
+    modernFooWithBool(sharemT);
+
+
+    std::unique_ptr<char> uP;
+    modernUniqueFooWithBool(uP);
     auto mT = modernFoo();
 
-    cout << "value of t " <<*mT<< endl;
+
+    std::unique_ptr<char> uP1;
+    std::unique_ptr<char> uP2;
+
+    modernUniqueMultiFooWithBool(uP1,uP2);
+
+    cout << "value of pointers: " <<*mT<<" "<< *sharemT<<" "<<*uP<<" "<<*uP1<<" "<<*uP2<<endl;
 
     delete t;
     return 0;
